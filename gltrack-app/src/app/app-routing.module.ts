a4dce5400ea89917606a3e0e5f0a95ae8db53ae1
Adrian Gonzalez <rckstr@outlook.com>
2019-06-18 21:38:59 -0700

    Routing and Nav work

--- gltrack-app/src/app/app-routing.module.ts
+++ gltrack-app/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Por default, redirigir al dashboard
   { path: '404', redirectTo: 'dashboard', pathMatch: 'full' }, // En caso de error 404, redirigir al dashboard
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'ingresos', component: RegistroComponent }
+  { path: 'registro', component: RegistroComponent }
 ];
 
 @NgModule({
--- gltrack-app/src/app/components/nav/nav.component.ts
+++ gltrack-app/src/app/components/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { NavMenuItem } from 'src/app/models/models';
 
 @Component({
   selector: 'app-nav',
@@ -9,12 +10,16 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+  menu = [
+    { Label: 'Dashboard', Target: '/dashboard' },
+    { Label: 'Registro', Target: '/registro' }
+  ] as NavMenuItem[];
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
 }
