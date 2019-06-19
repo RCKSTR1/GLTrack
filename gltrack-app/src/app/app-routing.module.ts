a838462743656f896b40d4d89603a8fe11abb6e7
Adrian Gonzalez <rckstr@outlook.com>
2019-06-18 21:21:44 -0700

    Routes

--- gltrack-app/src/app/app-routing.module.ts
+++ gltrack-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, // Por default, redirigir al dashboard
+  { path: '404', redirectTo: 'dashboard', pathMatch: 'full' }, // En caso de error 404, redirigir al dashboard
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'ingresos', component: RegistroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
