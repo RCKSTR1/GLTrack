8b8d9c4d98307e5606c848c2b66f9ac8a06d94fa
Adrian Gonzalez <rckstr@outlook.com>
2019-06-18 21:13:48 -0700

    Added components

--- gltrack-app/src/app/app.module.ts
+++ gltrack-app/src/app/app.module.ts
@@ -7,11 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './components/nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MaterialModule } from './modules/material/material.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    DashboardComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
